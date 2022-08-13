795073c73337df0e6fc3d42cd7e33ad9b0c91a14
Vitaliy <43200383+s04v@users.noreply.github.com>
2022-08-13 23:48:23 +0200

    Build endpoint

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const logger = require('morgan');
-const { exec } = require("child_process");
+const cors = require('cors');
+const { execSync } = require("child_process");
+const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.use(logger('dev'));
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.text());
 
-app.post('/build', (req, res) => {
-  console.log(req.body);
-
-});
-
-app.post('/run', (req, res) => {
-
-});
-
-
-exec("cat main.cpp", (error, stdout, stderr) => {
-  console.log(error);
-  console.log(stderr);
+app.post('/build', async (req, res) => {
+  const fileName = 'main.cpp';
+  const code = req.body;
+ 
+  console.log(code);
+  const ret = fs.writeFileSync(fileName, code);
+  const { stdout } = execSync(`bin/compiler ${filename}`);
+    
   console.log(stdout);
-})
+  res.status(200).send("OK");
+});
+
+// exec("cat main.cpp", (error, stdout, stderr) => {
+//   console.log(error);
+//   console.log(stderr);
+//   console.log(stdout);
+// })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
