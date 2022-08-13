e737f2677ae409c90e29a2610b713406af77ac5b
Vitaliy <43200383+s04v@users.noreply.github.com>
2022-08-14 00:37:49 +0200

    Fixes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { execSync } = require("child_process");
 const fs = require('fs');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 const corsOptions = {
   origin: "http://localhost:3000",
@@ -19,26 +19,19 @@ app.post('/build', async (req, res) => {
   const fileName = 'main.cpp';
   const code = req.body;
  
-  console.log(code);
   const ret = fs.writeFileSync(fileName, code);
   try {
     execSync(`compiler/compiler ${fileName}`);
   } catch(e) {
-    res.status(400).send({msg: e.stdout.toString()});
+    res.status(400).send({output: e.stdout.toString()});
+    return;
   }
 
   const cat = execSync(`cat output.asm`);
   const run = execSync('./a.out');
-  console.log(cat.stdout);
-  console.log(run.stdout);
-  res.status(200).send("OK");
+  res.status(200).send({output: run.toString(), asm: cat.toString()});
 });
 
-// exec("cat main.cpp", (error, stdout, stderr) => {
-//   console.log(error);
-//   console.log(stderr);
-//   console.log(stdout);
-// })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
