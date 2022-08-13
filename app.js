b77cfc7116fe6631ac9b434e182d04892f869d68
Vitaliy <43200383+s04v@users.noreply.github.com>
2022-08-13 21:32:20 +0200

    Project structure review

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const { exec } = require("child_process");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
+const port = 3000;
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.text());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.post('/build', (req, res) => {
+  console.log(req.body);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.post('/run', (req, res) => {
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
-module.exports = app;
+
+exec("cat main.cpp", (error, stdout, stderr) => {
+  console.log(error);
+  console.log(stderr);
+  console.log(stdout);
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
